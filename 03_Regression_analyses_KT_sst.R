e7fc3acd70052e6bc852f43dd27e8b83e663750f
Kira Turnham <1638089897113305@mil>
2025-06-20 16:05:53 -1000

    sst regression update

--- 03_Regression_analyses_KT_sst.R
+++ 03_Regression_analyses_KT_sst.R
@@ -55,9 +55,9 @@ large<- icra2025eds%>%
     .groups = "drop"
   )
 
-#######################################################################################
-### 1. looking at March site data by discrete SIZE CLASS #
-#######################################################################################
+###########################################
+### March site data by discrete SIZE CLASS#
+###########################################
 small_model<- glmmTMB(
   prop_mean_PM_adj ~ (SST_Mean + SST_range), #+ (1 | SITE),
   data = small,
@@ -97,14 +97,17 @@ site2<-site%>%
     # prop_mean_PM_adj = pmin(pmax(prop_mean_PM, epsilon), 1 - epsilon), #way to adjust 2
     SST_Mean = first(mean_Sea_Surface_Temperature_jplMUR_Daily_YR01),
     SST_range = first(mean_biweekly_range_Sea_Surface_Temperature_jplMUR_Daily_YR01),
+    Latitude= first(LATITUDE),
+    LONGITUDE = first(LONGITUDE),
     .groups = "drop")%>%
   mutate(
     prop_mean_PM_adj = case_when(  #way to adjust 3
       prop_mean_PM == 0 ~ 0.01,
-      prop_mean_PM == 1 ~ 0.99,
+      prop_mean_PM == 1 ~ 0.91,
       TRUE ~ prop_mean_PM
     )
   )
+
 
 hist(site2$prop_mean_PM, breaks = 30)
 hist(site2$prop_mean_PM_adj, breaks = 30)
@@ -116,4 +119,47 @@ model<- glmmTMB(
   family = beta_family()
 )
 summary(model)
+#SST_Mean:MED: coefficient=26.831 pvalue=0.025    
+
+############################################
+#look at residuals. first simulate w DHARMa#
+############################################
+
+sim_res <- simulateResiduals(fittedModel = model, plot = TRUE)
+#no significant deviations or problems detected
+hist(sim_res$scaledResiduals, main = "Histogram of Scaled Residuals", xlab = "Residuals")
+
+plotResiduals(sim_res, site2$SST_Mean)
+#plotResiduals(sim_res, site2$SST_range)
+
+testUniformity(sim_res)     # Are residuals uniformly distributed?
+#D = ns, KS test p = 0.53
+testDispersion(sim_res)     # Is there overdispersion?
+#p=0.528
+testOutliers(sim_res)       # Are there extreme values?
+#ns
+
+#plot resudials by site
+plotResiduals(sim_res, site2$SITE)
+#ns
+
+#Visualize interactions
+site2
+small_data <- subset(site2, TAIL_BINS == "Q20")
+newdata_small<-small_data
+newdata_small$SST_range <- mean(small_data$SST_range)  # Set SST_range to mean
+# newdata_small$SST_mean <- seq(min(small_data$SST_Mean), 
+#                                      max(small_data$SST_Mean), 
+#                                      by = round(diff(range(small_data$SST_Mean)), 3) / nrow(small_data))
+newdata_small$SST_mean <- seq(min(small_data$SST_Mean), 
+                              max(small_data$SST_Mean), 
+                              length.out = nrow(newdata_small)) 
+#this would be predicted PM , create lower and upper CI
+p <- predict(best.mod, newdata = newdata1, type = "response",se.fit=TRUE)
+p<-as.data.frame(p)
+colnames(p)<-c("Predicted_Juv","SE_Juv")
+newdata1<-cbind(newdata1,p)
+newdata1$Predict.lwr <- newdata1$Predicted_Juv - 1.96 * newdata1$SE_Juv # confidence interval upper bound
+newdata1$Predict.upr <- newdata1$Predicted_Juv + 1.96 * newdata1$SE_Juv # confidence interval lower bound
+newdata1$HSts_cat<-"0-3 years"
 
